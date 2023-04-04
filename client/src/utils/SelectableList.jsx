fef43da7857175a339a22cf09cf1a07ce54f35c5
Jingshi Liu <jingshiliu07@gmail.com>
2023-04-03 22:33:47 -0400

    renamed variable 'options' to 'listValues'

--- client/src/utils/SelectableList.jsx
+++ client/src/utils/SelectableList.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 function SelectableList(props) {
-    const { options, onChange } = props;
+    const { listValues, onChange } = props;
 
     const handleChange = (event) => {
         onChange(event.target.value);
     };
 
     return (
-        <select onChange={handleChange}>
-            {options.map((option) => (
+        <select className={'SelectableList'} onChange={handleChange}>
+            {listValues.map((option) => (
                 <option key={option.value} value={option.value}>
                     {option.label}
                 </option>
