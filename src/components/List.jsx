c68f1f2e33ecafaa11de69e73df8a9ddd76eb35c
Jingshi Liu <jingshiliu07@gmail.com>
2023-05-04 16:38:42 -0400

    delete: removed all code inside List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import styled from "styled-components";
 
-import PlaceDetails from "./PlaceDetails.jsx";
 import SelectableList from "../utils/SelectableList.jsx";
 
 const StyledList = styled.div`
@@ -9,55 +8,12 @@ const StyledList = styled.div`
 `;
 
 
-function List({places}) {
+function List({}) {
 
-    const [type, setType] = useState('restaurants');
-    const [rating, setRating] = useState(0);
-
-    const placePrompt = {
-        'restaurants': 'Food & Dining',
-        'hotels': 'Hotels',
-        'attractions': 'Attractions'
-    }
 
     return (
-        <StyledList>
-            <h1>{placePrompt[type]} around you</h1>
-            <form>
-                <label>
-                    Location:
-                    <SelectableList
-                        options={[
-                            {value: 'restaurants', label: 'Restaurants'},
-                            {value: 'hotels', label: 'Hotels'},
-                            {value: 'attractions', label: 'Attractions'},
-                        ]}
-                        onChange={value => setType(value)}
-                    />
-                </label>
+        <StyledList className={'List'}>
 
-                <label>
-                    Rating:
-                    <SelectableList
-                        options={[
-                            {value: 4.5, label: 'Above 4.5'},
-                            {value: 0, label: 'All'},
-                            {value: 4, label: 'Above 4.0'},
-                            {value: 3, label: 'Above 3.0'},
-                            {value: 2, label: 'Above 2.0'},
-                        ]}
-                        onChange={value => setRating(value)}
-                    />
-                </label>
-            </form>
-
-            <section className={'PlaceList'}>
-                {places?.map((place, i) => (
-                    <div className={'PlaceListItem'} key={i}>
-                        <PlaceDetails place={place}/>
-                    </div>
-                ))}
-            </section>
         </StyledList>
     );
 }
