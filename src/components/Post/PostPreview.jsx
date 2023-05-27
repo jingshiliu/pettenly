20af64bd6a385a56c47f79a87e5b51adbd2a4447
Jingshi Liu <jingshiliu07@gmail.com>
2023-05-26 21:55:30 -0400

    feat: implement first version of PostPreview

--- src/components/Post/PostPreview.jsx
+++ src/components/Post/PostPreview.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import styled from "styled-components";
+import {AuthContext} from "../../context/AuthContext.jsx";
 
-function PostPreview({}) {
+
+const StyledPostPreview = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: red;
+`
+
+function PostPreview({setDisplayingPost, post}) {
+    const {isLoggedIn} = useContext(AuthContext)
+
+    const handleOnClick = isLoggedIn ? () => setDisplayingPost(post) : null;
     return (
-        <div></div>
+        <StyledPostPreview onClick={handleOnClick}>
+            Super Cute Pet Image
+        </StyledPostPreview>
     );
 }
 
