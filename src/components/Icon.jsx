fa25505193900adf5cb6d1c47d0cfff11191372f
Jingshi Liu <jingshiliu07@gmail.com>
2023-05-27 16:13:38 -0400

    feat(Icon): implement Icon which uses FaCat from react-icons

---
+++ src/components/Icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {FaCat} from 'react-icons/fa'
+function Icon() {
+    return (
+        <span className={'Icon'}>
+            <FaCat />
+        </span>
+    );
+}
+
+export default Icon;
