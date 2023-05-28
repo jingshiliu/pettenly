34f2a4fc398a8e21f2d43a17d53df05798d11679
Jingshi Liu <jingshiliu07@gmail.com>
2023-05-27 22:18:48 -0400

    feat(PostPreview): implement displaying photo

--- src/components/Post/PostPreview.jsx
+++ src/components/Post/PostPreview.jsx
@@ -1,21 +1,48 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import styled from "styled-components";
 import {AuthContext} from "../../context/AuthContext.jsx";
+import {getImageFromStorage} from "../../utils/index.js";
 
 
 const StyledPostPreview = styled.div`
   width: 50px;
   height: 50px;
-  background-color: red;
+  background-color: ${({theme}) => theme.colors.greenBlue};
+  object-fit: contain;
+  overflow: hidden;
+  
+  border-radius: 1em;
+  
+  img{
+    width: inherit;
+    height: inherit;
+  }
 `
 
 function PostPreview({setDisplayingPost, post}) {
+    const [imageUrl, setImageUrl] = useState('')
     const {isLoggedIn} = useContext(AuthContext)
+
+    useEffect(()=>{
+        getImage()
+    }, [])
+
+    async function getImage(){
+        const url = await getImageFromStorage(post.petImage)
+        if(! url) return
+        setImageUrl(url)
+    }
+
 
     const handleOnClick = isLoggedIn ? () => setDisplayingPost(post) : null;
     return (
         <StyledPostPreview onClick={handleOnClick}>
-            Super Cute Pet Image
+            {
+                imageUrl ?
+                    <img src={imageUrl} alt="preview photo of post that is a pet"/>
+                    :
+                    "Super Cute Pet Image"
+            }
         </StyledPostPreview>
     );
 }
