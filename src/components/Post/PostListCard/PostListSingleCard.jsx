fd246aa504496d40a0a1fbd2bdb8d809591ff228
Jingshi Liu <jingshiliu07@gmail.com>
2023-06-12 20:15:40 -0400

    chore [PostListCard]: create a file for PostListSingleCard

--- src/components/Post/PostListCard/PostListSingleCard.jsx
+++ src/components/Post/PostListCard/PostListSingleCard.jsx
@@ -1,9 +1,7 @@
-import React, {useContext, useEffect, useState} from 'react';
 import styled from "styled-components";
-import {getImageFromStorage, getPosts, getUser} from "../../utils/index.js";
-import {nanoid} from "nanoid";
-import {ListContext} from "../../context/ListContext.js";
-import PostDetail from "./PostDetail.jsx";
+import React, {useContext, useEffect, useState} from "react";
+import {getImageFromStorage} from "../../../utils/index.js";
+import {ListContext} from "../../../context/ListContext.js";
 
 const StyledPostSingleCard = styled.div`
   margin: 1em 0;
@@ -117,69 +115,4 @@ function PostListSingleCard({post}) {
     );
 }
 
-
-const StyledPostListAllCard = styled.div`
-  width: 19vw;
-  height: 75vh;
-  border-radius: 1.4em;
-  background-color: ${({theme}) => theme.colors.deepGreenBlue};
-  padding: 20px;
-  color: ${({theme}) => theme.colors.lightGreen};
-  opacity: 0.94;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  overflow-x: hidden;
-  
-  h2{
-    font-weight: 400;
-  }
-  
-  hr{
-    border: none;
-    border-bottom: 1px solid ${({theme}) => theme.colors.lightGreen};
-  }
-  
-  .postsContainer{
-    overflow: scroll;
-  }
-`
-
-function PostListAllCard({postList, userData}) {
-    const [posts, setPosts] = useState(postList)
-    const [user, setUser] = useState(userData)
-
-    useEffect(()=>{
-        if(! postList){
-            getPosts()
-                .then(postsData => setPosts(postsData))
-                .catch(err => console.error(err))
-        }
-
-        if(! userData){
-            getUser()
-                .then(userData =>{
-                    setUser(userData)
-                })
-                .catch(err => console.error(err))
-        }
-    }, [])
-
-    console.log(posts)
-
-    return (
-        <StyledPostListAllCard>
-            <div className="cardTitle">
-                <h2>
-                    {user.username}'s Post
-                </h2>
-                <hr/>
-            </div>
-            <div className="postsContainer">
-                {posts.map(post => <PostListSingleCard post={post} key={nanoid()}/>)}
-            </div>
-        </StyledPostListAllCard>
-    );
-}
-
-export default PostListAllCard;
+export default PostListSingleCard;
