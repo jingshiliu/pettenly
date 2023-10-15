ee51debfe6681e83593d8a61955782a0046bf5eb
Jingshi Liu <jingshiliu07@gmail.com>
2023-10-15 19:17:37 -0400

    chore(Messenger): implement ChatPreview section and loadData() for chatPreviews only

--- src/components/Messenger/ChatPreview.jsx
+++ src/components/Messenger/ChatPreview.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import styled from "styled-components";
 
 const StyledChatPreview = styled.div`
-
+  width: 100%;
+  background-color: ${({theme}) => theme.colors.blue};
+  color: ${({theme}) => theme.colors.deepGreenBlue};
+  padding: 20px 10px;
+  border-radius: 0.5em;
+  :hover{
+    filter: brightness(90%);
+  }
 `
 
-function ChatPreview({receiver, setCurrentChat, chatId}) {
+function ChatPreview({receiverUserName, setCurrentChat, chatId}) {
     return (
         <StyledChatPreview onClick={() => setCurrentChat(chatId)}>
-            {receiver.username}
+            {receiverUserName}
         </StyledChatPreview>
     );
 }
--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
+
+import {getChatPreviews, getUser} from "../../utils/index.js";
+import {auth} from "../../config/firebase.js";
+import ChatPreview from "./ChatPreview.jsx";
 
 const StyledChat = styled.div`
   width: 50vw;
@@ -9,16 +13,71 @@ const StyledChat = styled.div`
   z-index: 1;
   padding: 20px;
   color: ${({theme}) => theme.colors.lightGreen};
+  display: flex;
+  
+  .chatPreview{
+    width: 30%;
+    
+    h1{
+      margin-bottom: 10px;
+    }
+  }
+  
+  .chat{
+    width: 70%;
+  }
 `
 
 function Messenger() {
     // two columns, left for chat selection with diff user, right for current chat
     // when component first load, fetch all the chats of current user from firebase
     // sorted with the order of last updated
+    const [chats, setChats] = useState({})
+    const [currentChat, setCurrentChat] = useState({})
+    const userId = auth?.currentUser?.uid
+    console.log(currentChat)
+
+
+    useEffect(()=>{
+        loadData()
+            .catch(err => console.error(err))
+    }, [])
+
+    async function loadData(){
+        const chatPreviews = await getChatPreviews()
+        const newChats = {}
+        for(let chatPreview of chatPreviews){
+            let chatId = chatPreview.id
+            let receiverId = chatPreview.user1 === userId ? chatPreview.user2 : chatPreview.user1
+            let receiver = await getUser(receiverId)
+            newChats[chatId] = {
+                ...chatPreview,
+                receiverId,
+                receiverEmail: receiver.email,
+                receiverImage: receiver.image,
+                receiverUserName: receiver.username
+            }
+        }
+        setChats(newChats)
+        setCurrentChat(newChats[chatPreviews[0].id])
+    }
 
     return (
         <StyledChat>
-            <h1>I'm a chat</h1>
+            <section className={'chatPreview'}>
+                <h1>Messenger</h1>
+                {Object.values(chats).map(chat =>
+                    <ChatPreview
+                        receiverUserName={chat.receiverUserName}
+                        setCurrentChat={setCurrentChat}
+                        chatId={chat.id}
+                        key={chat.id}
+                    />
+                )}
+            </section>
+            <section className={'chat'}>
+
+            </section>
         </StyledChat>
     );
 }
