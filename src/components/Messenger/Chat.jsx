5c92a329cdb4235b4ecf47a8ef7235fd0435d8f6
Jingshi Liu <jingshiliu07@gmail.com>
2023-10-20 01:46:39 -0400

    feat(Messenger|Chat): create component Chat

--- src/components/Messenger/Chat.jsx
+++ src/components/Messenger/Chat.jsx
@@ -1,16 +1,40 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
+import {subscribeToMessageUpdates} from "../../utils/index.js";
+import ChatMessage from "./ChatMessage.jsx";
 
 const StyledChat = styled.div`
 
 `
 
-function Chat({chatId}) {
+function Chat({chatId, chatInfo, user}) {
     const [messages, setMessages] = useState([])
+
+    useEffect(()=>{
+        const unsubscribe = subscribeToMessageUpdates(chatId, newMessages =>{
+            setMessages(newMessages)
+        })
+
+        return ()=>{
+            if(unsubscribe !== undefined){
+                unsubscribe()
+            }
+        }
+    }, [chatId])
 
     return (
         <StyledChat>
-            hello, {chatId}
+            <h1>Hello, {chatId}</h1>
+            {
+                messages.map(message => {
+                    return <ChatMessage
+                                key={message.id}
+                                message={message}
+                                chatBuddy={chatInfo.chatBuddy}
+                                user={user}
+                           />
+                })
+            }
         </StyledChat>
     );
 }
