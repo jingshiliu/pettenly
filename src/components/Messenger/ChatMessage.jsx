85f1dadd7d91ba80c18905c3233ccc3f4e790b5c
Jingshi Liu <jingshiliu07@gmail.com>
2023-10-20 02:21:33 -0400

    chore(Messenger|ChatMessage): style the component

--- src/components/Messenger/ChatMessage.jsx
+++ src/components/Messenger/ChatMessage.jsx
@@ -3,32 +3,58 @@ import styled from "styled-components";
 import {auth} from "../../config/firebase.js";
 
 const StyledChatMessage = styled.div`
+  img{
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+    border-radius: 20%;
+  }
   
+  .messageBox{
+    width: 100%;
+    display: flex;
+    
+    .messageContainer{
+      display: inline-block;
+      align-self: center;
+      padding: 0.2em 0.5em;
+      transform: translateY(-0.2em);
+    }
+  }
+  
+  .user{
+    justify-content: end;
+  }
+  
+  .chatBuddy{
+
+  }
 `
 
 function ChatMessage({chatBuddy, message, user}) {
     let Message;
-    if(chatBuddy.id === user.id){
+    if(message.sender === user.id){
         Message = (
-            <div className={'user'}>
-                <span>{message.message}</span>
+            <div className={'user messageBox'}>
+                <div className="messageContainer">
+                    <span>{message.message}</span>
+                </div>
                 <img src={user.image} alt={'Profile image of user'}/>
             </div>
         )
     }else{
         Message = (
-            <div className={'chatBuddy'}>
-                <div className={'info'}>
-                    <span>{chatBuddy.name}</span>
-                    <img src={chatBuddy.image} alt="Profile image of other people"/>
+            <div className={'chatBuddy messageBox'}>
+                <img src={chatBuddy.image} alt="Profile image of other people"/>
+                <div className="messageContainer">
+                    <span>{message.message}</span>
                 </div>
-                <span>{message.message}</span>
             </div>
         )
     }
 
     return (
-        <StyledChatMessage>
+        <StyledChatMessage className={'ChatMessage'}>
             {Message}
         </StyledChatMessage>
     );
