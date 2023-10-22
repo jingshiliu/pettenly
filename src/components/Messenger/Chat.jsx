ed64fcc75ee05507cc2e2f0dc35d2006ef4d1dec
Jingshi Liu <jingshiliu07@gmail.com>
2023-10-22 19:25:00 -0400

    feat(Messenger): implement UI of message box

--- src/components/Messenger/Chat.jsx
+++ src/components/Messenger/Chat.jsx
@@ -1,23 +1,58 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styled from "styled-components";
 import {subscribeToMessageUpdates} from "../../utils/index.js";
 import ChatMessage from "./ChatMessage.jsx";
 
 const StyledChat = styled.div`
+  height: 100%;
+  
   h1{
     font-weight: bolder;
 
   }
   .messagesContainer{
     padding: 1em 0;
+    height: 75%;
+    
     .ChatMessage{
       margin-bottom: 1em;
+    }
+  }
+  
+  .messageSender{
+    width: 100%;
+    position: relative;
+    
+    textarea{
+      border: none;
+      width: 100%;
+      padding: 0.8em 0.5em;
+      box-sizing: border-box;
+      border-radius: 1em;
+      color: ${({theme}) => theme.colors.deepGreenBlue2};
+      background-color: ${({theme}) => theme.colors.lightGreen};
+    }
+    
+    button{
+      border-radius: 0.8em;
+      background-color: ${({theme}) => theme.colors.deepGreenBlue2};
+      color: ${({theme}) => theme.colors.lightGreen};
+      padding: 0.5em 2em;
+      
+      position: absolute;
+      right: 0;
+      bottom: 0.2em;
+      
+      :hover{
+        filter: brightness(0.9);
+      }
     }
   }
 `
 
 function Chat({chatId, chatInfo, user}) {
     const [messages, setMessages] = useState([])
+    const messageRef = useRef()
 
     useEffect(()=>{
         const unsubscribe = subscribeToMessageUpdates(chatId, newMessages =>{
@@ -45,6 +80,10 @@ function Chat({chatId, chatInfo, user}) {
                         />
                     })
                 }
+            </section>
+            <section className="messageSender">
+                <textarea ref={messageRef} name="" id="" cols="30" rows="5" placeholder={'Type your message here'}/>
+                <button>Send</button>
             </section>
         </StyledChat>
     );
